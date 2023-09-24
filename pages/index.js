2d66e5ca1a6d524dc04d2fb7fea4ae523910170d
Vitor Lopes <48527153+vitorglopes@users.noreply.github.com>
2023-09-24 14:01:34 -0300

    feat: update index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Olá, muito bom ver você por aqui!</h1>
+      <h3>
+        Por enquanto esse site está em construção, mas você pode me seguir lá no
+        <a href={"https://www.linkedin.com/in/vitorglopes/"}> LinkedIn</a>
+      </h3>
+      <h4>Até mais!</h4>
+    </>
+  );
 }
 
 export default Home;
